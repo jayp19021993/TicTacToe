146f697e0344d6bd6d09eaaf881846c57fb57f4e
Jay Patel <jay_patel_123@hotmail.co.uk>
2022-05-22 13:12:02 +0100

    board update

--- src/components/board.js
+++ src/components/board.js
@@ -3,6 +3,10 @@ import Square from "./square";
 
 const Board = ({Square,onClick}) =>(
     <div className="board">
-        {squares.map}
+        {squares.map((square,i)=>(
+            <square key={i} value={square} onClick={() => onClick(i)}/>        
+            ))}
     </div>
-)
+); 
+
+export default Board;
