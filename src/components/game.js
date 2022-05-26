e6669be9bbbdc0624594851fdeb8642f95d84dd9
Jay Patel <jay_patel_123@hotmail.co.uk>
2022-05-26 15:22:49 +0100

    game update

--- src/components/game.js
+++ src/components/game.js
@@ -2,6 +2,7 @@ import React,{useStae} from "react";
 import Board from "./board"; 
 
 const Game=() =>{
-   const [history, setHistory] = useStae([Array(9).fill(null)]);
-   const
+   const [history, setHistory] = useState([Array(9).fill(null)]);
+   const [setNumber, setStepNumber ] = useState(0);
+   const [xIsNext,setXisNext] = 
 }   
