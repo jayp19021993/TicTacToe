5ac7b2679bf388c18fb1dcec8eaf82aaa1da5dcd
Jay Patel <jay_patel_123@hotmail.co.uk>
2022-05-31 17:59:33 +0100

    game update

--- src/components/game.js
+++ src/components/game.js
@@ -1,8 +1,10 @@
 import React,{useStae} from "react";
+import {calculateWinner } from "../helper";
 import Board from "./board"; 
 
 const Game=() =>{
    const [history, setHistory] = useState([Array(9).fill(null)]);
    const [setNumber, setStepNumber ] = useState(0);
    const [xIsNext,setXisNext] = useState(true);
+   const winner = 
 }   
